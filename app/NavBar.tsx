e53b8e87fb266bbd3323a23172de8b2868249704
Simon-Nguyen <114342975+simon-sgn@users.noreply.github.com>
2023-11-11 19:06:10 +0700

    Style the active link in the navigation bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,15 @@
+// The usePathname hook is dependent on browser API, so this component needs to be a client component
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -17,7 +24,11 @@ const NavBar = () => {
         {links.map((link) => (
           <li key={link.href}>
             <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              className={classnames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
               href={link.href}
             >
               {link.label}
